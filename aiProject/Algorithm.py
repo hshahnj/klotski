570d8e80a8784893ce795582e9c52a5c9596347d
hshahnj <shah.harshil77@gmail.com>
2019-11-10 23:04:33 -0500

    first commit

---
+++ aiProject/Algorithm.py
@@ -0,0 +1,124 @@
+import sys
+import importlib
+
+
+#INIT
+Problem = importlib.import_module(sys.argv[1]) #Import Module
+heuristic_choice = sys.argv[2]
+Initial_state = importlib.import_module(sys.argv[3])
+init_state = Initial_state.CREATE_INITIAL_STATE()
+heuristics = Problem.HEURISTICS[heuristic_choice]
+
+#Counting # of iterations
+COUNT = None
+
+#To trace steps
+BACKLINKS = {}
+
+def runAStar():
+    print("Initial State:")
+    print(Problem.printCurrentState(init_state))
+    global COUNT, BACKLINKS
+    COUNT = 0
+    BACKLINKS = {}
+    IterateAStar(init_state)
+    print(str(COUNT)+" states examined.")
+
+
+# iterates through initial state using A* with given hueristics
+# to solve until goal state
+def IterateAStar(initial_state):
+    global COUNT, BACKLINKS
+
+    OPEN = [[initial_state, heuristics(initial_state)]]
+    CLOSED = []
+
+    # print(initial_state)
+    COST = {Problem.HASHCODE(initial_state): 0}
+
+    BACKLINKS[Problem.HASHCODE(initial_state)] = -1
+
+    while OPEN != []:
+        S = OPEN[0]
+        del OPEN[0]
+        CLOSED.append(S)
+
+        if Problem.GOAL_TEST(S[0]):
+            print(Problem.GOAL_MESSAGE_FUNCTION(S[0]))
+            backtrace(S[0])
+            return
+
+        COUNT += 1
+        if (COUNT % 32)==0:
+            print(".",end="")
+            if (COUNT % 128)==0:
+                print("COUNT = "+str(COUNT))
+                print("len(OPEN)="+str(len(OPEN)))
+                print("len(CLOSED)="+str(len(CLOSED)))
+        L = []
+
+        # for each possible child in S (state)
+        for op in Problem.OPERATORS:
+            # is a child
+            if op.precond(S[0]):
+                new_state = op.state_transf(S[0])
+
+                # index of occurrence in CLOSED
+                occur_closed = occurs_in(new_state, CLOSED)
+
+                # index of occurence in OPEN
+                occur_open = occurs_in(new_state, OPEN)
+
+                # the moves made so far + 1
+                new_cost = COST[Problem.HASHCODE(S[0])] + 1
+                # place in neighbor if new state
+                if occur_closed == -1 and occur_open == -1:
+                    L.append([new_state, heuristics(new_state)])
+                    BACKLINKS[Problem.HASHCODE(new_state)] = S[0]
+                    COST[Problem.HASHCODE(new_state)] = new_cost
+
+                elif occur_open > -1:
+                    # check to see if this move is more efficient
+                    if COST[Problem.HASHCODE(new_state)] > new_cost:
+                        COST[Problem.HASHCODE(new_state)] = new_cost
+                        OPEN[occur_open] = [new_state, new_cost]
+
+        # for all neighbors found, if it equals to states in OPEN,
+        # delete it, shouldn't occur but juuuuust in case...
+        for s2 in L:
+            for i in range(len(OPEN)):
+                if Problem.DEEP_EQUALS(s2, OPEN[i][0]):
+                    del OPEN[i]; break
+
+        OPEN = L + OPEN
+        OPEN.sort(key=lambda x: x[1])
+
+# determines if the given state is equal to any of the state
+# within the list and returns the index if it does exist
+# otherwise, return -1 implying it wasn't found
+def occurs_in(s1, lst):
+    index = 0
+    for s2 in lst:
+        if Problem.DEEP_EQUALS(s1, s2[0]):
+            return index
+        else:
+            index = index + 1
+    return -1
+
+
+def backtrace(S):
+    global BACKLINKS
+
+    path = []
+    while not S == -1:
+        path.append(S)
+        S = BACKLINKS[Problem.HASHCODE(S)]
+    path.reverse()
+    print("Solution path: ")
+    for s in path:
+        print(Problem.DESCRIBE_STATE(s))
+    return path   
+
+
+if __name__=='__main__':
+    runAStar()
---
+++ aiProject/KlotskiStates.py
@@ -0,0 +1,275 @@
+# The goal of the game is to manuever the tiles by "swapping" with the empty spaces
+# in such a way that the 2x2 block is located at the center bottom in order to remove that
+# tile in the "exit" opening at the center bottom of the board.
+
+
+#<INITIAL_STATE>
+GOAL_BLOCK = 'A'
+CREATE_INITIAL_STATE = lambda : ["B", "A", "A", "C",
+                                 "B", "A", "A", "C",
+                                 "D", "E", "E", "F",
+                                 "D", "G", "H", "F",
+                                 "I", "_", "_", "J"]
+#</INITIAL_STATE>
+
+
+
+# id = ALPHA_CHAR
+# x = top left starting x
+# y = top left starting y
+# w = how wide to the right
+# h = how long to the bottom
+
+# defining can move precond
+class Piece:
+    # constructor
+    def __init__(self, id, x, y, w, h):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    # string representation of the piece
+    # for DEBUG purposes
+    def __str__(self):
+        return self.id + " " + str(int(self.x)) + " " + str(int(self.y)) \
+        + " " + str(int(self.w)) + " " + str(int(self.h))
+
+    # Algorithm to determine whether we can move or not
+    # precond defining whether or not this piece can move in a certain
+    # direction given a current state
+    def can_move(self, state, direction):
+        w = int(self.w)
+        h = int(self.h)
+        x = int(self.x)
+        y = int(self.y)
+        try:
+            move = 1
+            if direction == 1 or direction == 2:
+                move = -1
+            if direction == 0:
+                move += w - 1
+            if direction == 3:
+                move += h - 1
+            if direction % 2 == 0:
+                for i in range(h):
+                    if 0 <= x + move < 4:
+                        if state[4*(y + i) + x + move] != "_":
+                            return False
+                    else:
+                        return False
+            else:
+                for i in range(w):
+                    if 0 <= y + move < 5:
+                        if state[4*(y + move) + (x + i)] != "_":
+                            return False
+                    else:
+                        return False
+            return True
+
+        except (Exception) as e:
+            print(e)
+
+
+
+# returns a message when the goal is reached
+def goal_message(s):
+    return "Solved!"
+
+
+
+# Performs an appropriately deep copy of a state,
+# for use by operators in creating new states.
+def copy_state(s):
+    new_state = list(s)
+    return new_state
+
+# determines whether or not the two states are indentical by value
+def DEEP_EQUALS(s1, s2):
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            return False
+    return True
+
+
+# string representation of the state of the game as shown:
+# A B B C
+# A B B C
+# D E E F
+# D G H F
+# I _ _ J
+def printCurrentState(state):
+    result = ""
+    for row in range(5):
+        result = result + "   "
+        for col in range (4):
+            result = result + (str(state[4 * row + col]) + " ")
+        result = result + "\n"
+    return result
+
+
+# hashes the current state given such that all states
+# are guaranteed to be unique
+def HASHCODE(state):
+    return printCurrentState(state)
+
+
+
+class Operator:
+    def __init__(self, name, precond, state_transf):
+        self.name = name
+        self.precond = precond
+        self.state_transf = state_transf
+
+    def is_applicable(self, s):
+        return self.precond(s)
+
+    def apply(self, s):
+        return self.state_transf(s)
+
+
+# moves a tile in the given direction in the current state given
+# and returns that new state
+def move(s, tile, dir):
+    '''Assuming it's legal to make the move, this computes
+     the new state resulting from moving the a tile to an available
+     location'''
+    new_state = copy_state(s)
+
+    curr_index = tile.y * 4 + tile.x
+    for width in range(tile.w):
+        for height in range(tile.h):
+            tile_index = int(curr_index + width + height * 4)
+
+            if dir == 3:
+                new_state[tile_index + 4] = s[tile_index]
+                if height == 0:
+                    new_state[tile_index] = "_"
+
+            elif dir == 1:
+                new_state[tile_index - 4] = s[tile_index]
+                if height == tile.h - 1:
+                    new_state[tile_index] = "_"
+
+            elif dir == 0:
+                new_state[tile_index + 1] = s[tile_index]
+                if width == 0 :
+                    new_state[tile_index] = "_"
+
+            else:
+                new_state[tile_index - 1] = s[tile_index]
+                if width == tile.w - 1:
+                    new_state[tile_index] = "_"
+
+
+    return new_state
+
+
+# determines whether or not the state is a goal state
+# implying that 2x2 square is on the center bottom
+def goal_test(state):
+    return state[13] == GOAL_BLOCK and state[14] == GOAL_BLOCK and \
+           state[17] == GOAL_BLOCK and state[18] == GOAL_BLOCK
+
+
+
+# translate a numerical direction into the corresponding
+# compass direction in the following format:
+#    1
+# 2     0
+#    3
+# on the condition that only 0 - 3 is passed in
+def translate_dir(num):
+    dir = ''
+    if num == 0:
+        dir = 'east'
+    elif num == 1:
+        dir = 'north'
+    elif num == 2:
+        dir = 'west'
+    else:
+        dir = 'south'
+    return dir
+
+
+
+# returns the can_move function of the given piece in a
+# direction relative to the state
+def can_move(s, piece, direction):
+    return piece.can_move(s, direction)
+
+
+
+# creates a list of all possible combinations of tiles to
+# direction = (0, 1, 2, 3) and returns it
+def combo_list():
+    ls = []
+    for tile in sorted(list(set(CREATE_INITIAL_STATE()) - set(['_']))):
+        for i in range(4):
+            ls.append((tile, i))
+    return ls
+
+
+# makes a piece object given a tile and the state locating that tile
+# and returns it
+def make_piece(state, tile):
+    index = state.index(tile)
+    curr_col = index % 4
+    curr_row = int(index / 4)
+
+    shape = [1,1]
+    if state[(index + 1)%len(state)] == state[index]: #finds the axb value
+        shape[0] += 1
+    if state[(index + 4)%len(state)] == state[index]:
+        shape[1] += 1
+    return Piece(state[index], curr_col, curr_row, shape[0], shape[1])
+
+
+
+#<OPERATORS>
+OPERATORS = [Operator("Move tile " + str(tile) + " to the " + str(translate_dir(direction)),
+                      lambda s, p=tile, q=direction: can_move(s, make_piece(s, p), q),
+                      lambda s, p=tile, q=direction: move(s, make_piece(s, p), q))
+             for (tile, direction) in combo_list()]
+#</OPERATORS>
+
+
+#<GOAL_TEST> (optional)
+GOAL_TEST = lambda s: goal_test(s)
+#</GOAL_TEST>
+
+
+#<GOAL_MESSAGE_FUNCTION> (optional)
+GOAL_MESSAGE_FUNCTION = lambda s: goal_message(s)
+#</GOAL_MESSAGE_FUNCTION>
+
+
+
+# totals up the number of tile square beneath the goal block
+# including whether or not the empty spaces are adjacent
+# and returns that value
+def h_custom(s):
+    goal_y = int(s.index(GOAL_BLOCK) / 4) + 1
+    total = 0
+    empty_index = int(s.index('_'))
+    if ((empty_index+1)%4 != 0 and s[(empty_index+1)%4] != '_') \
+            or (s[(empty_index + 4) % 20] != '_'):
+        total += 2
+
+    for key in sorted(set(s) - set(['_'])): #all letters
+        piece = make_piece(s, key) #now you have piece object
+        if (piece.y > goal_y):
+            total += piece.w * piece.h
+    return int(total)
+
+
+
+#<HEURISTICS>
+HEURISTICS = {'h_custom':h_custom}
+#</HEURISTICS>
+
+# <STATE_VIS>
+def render_state(s):
+    return printCurrentState(s)
+# </STATE_VIS>
